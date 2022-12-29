099302e6c4e484d0a4bb7154a9e98c7935bd5449
Jorge Tinoco <jorgel.tinoco@gmail.com>
2022-12-29 14:29:06 -0600

    Add business and model layer

---
+++ AspNetCoreMvcWebApp.Business/StudentBusiness.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreMvcWebApp.Models;
+
+namespace AspNetCoreMvcWebApp.Business
+{
+    /// <summary>
+    /// Business logic for the student.
+    /// </summary>
+    public class StudentBusiness
+    {
+        /// <summary>
+        /// Return some student information.
+        /// </summary>
+        /// <param name="studentId">Student ID used to search his/her information.</param>
+        /// <returns>Student details.</returns>
+        public StudentsDetail GetStudentDetail(int studentId)
+        {
+            var fakeDetail = new StudentsDetail();
+            fakeDetail.Name = "Fake And Static name";
+            fakeDetail.Courses = new List<string> { "FakeCourse1", "FakeCourse 2" };
+            return fakeDetail;
+        }
+
+    }
+}
---
+++ AspNetCoreMvcWebApp.Models/StudentDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace AspNetCoreMvcWebApp.Models
+{
+    /// <summary>
+    /// VieModel containing some student detail.
+    /// </summary>
+    public class StudentsDetail
+    {
+        /// <summary>
+        /// Student name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// All the courses where the student is involved
+        /// </summary>
+        public IEnumerable<string> Courses { get; set; }
+    }
+}
